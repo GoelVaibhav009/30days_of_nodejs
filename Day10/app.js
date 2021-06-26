1eeb81c146528a4ca0ba93778a1e3e701cca8d63
GoelVaibhav009 <creativeprogrammer623@gmail.com>
2021-06-26 18:09:07 +0530

    Day 11 & day 12

--- Day10/app.js
+++ Day10/app.js
@@ -1,20 +1,67 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const multer = require("multer");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, res, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, res, cb) => {
+  if (
+    file.mimetype === "images/png" ||
+    file.mimetype === "images/jpg" ||
+    file.mimetype === "images/jpeg"
+  )
+    cb(null, true);
+  else cb(null, false);
+};
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single("images")
+);
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message || "error found";
+  res.status(status).json({
+    message: message,
+  });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://tryboy:123@cluster0.ywzyd.mongodb.net/demo?retryWrites=true&w=majority",
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then((result) => app.listen(8080))
+  .catch((err) => console.log(err));
--- Day10/controllers/feed.js
+++ Day10/controllers/feed.js
@@ -1,26 +1,52 @@
+const { validationResult } = require("express-validator/check");
+const Post = require('../models/post')
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/node.jpg',
-        creator: {
-          name: 'Vaibhav'
-        },
-        createdAt: new Date()
-      }
-    ]
-  });
+  Post.find().then(posts => {
+    res.status(200).json({
+      message:'Data Fetched',
+      posts: posts
+    })
+  }).catch(err=>{
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  })
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered incorrect data");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+  // console.log(req.file.path);
+  // const imageUrl = req.file.path;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'imageUrl',
+    creator: {
+      name: "Vaibhav Goel",
+    },
   });
+  // Create post in db
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: { id: new Date().toISOString(), title: title, content: content },
+      })
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
