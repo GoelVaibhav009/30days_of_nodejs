8a1b16584eea601a702c68b1f625c33e90194e5d
GoelVaibhav009 <creativeprogrammer623@gmail.com>
2021-07-02 19:16:58 +0530

    Till Day 19

--- Rest Api Frontend/src/pages/Feed/Feed.js
+++ Rest Api Frontend/src/pages/Feed/Feed.js
@@ -50,7 +50,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page)
+    fetch('http://localhost:8080/feed/posts?page=' + page, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -109,16 +113,19 @@ class Feed extends Component {
     formData.append('title', postData.title);
     formData.append('content', postData.content);
     formData.append('image', postData.image);
-    let url = 'http://localhost:8080/feed/post';
+    let url = 'http://localhost:8080/feed/post/';
     let method = 'POST';
     if (this.state.editPost) {
-      url = 'http://localhost:8080/feed/post' + this.state.editPost._id;
+      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
       method= "PUT";
     }
 
     fetch(url, {
       method: method,
-      body: formData
+      body: formData,
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -171,7 +178,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/'+ postId, {
-      method: "DELETE"
+      method: "DELETE",
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
